217f7fbcb894c9a66e1fa030406a07ff7f66c6e7
Yohannes Sisay <rasjohnimarley@gmail.com>
2020-04-05 15:17:32 -0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+          'host' => env('DB_HOST', 'sql210.epizy.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'epiz_25470642_blog'),
+            'username' => env('DB_USERNAME', 'epiz_25470642'),
+            'password' => env('DB_PASSWORD', 'XMktKgdqxKY'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
